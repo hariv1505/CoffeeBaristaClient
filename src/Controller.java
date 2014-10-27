952a085af14c0f9f8041521cd6b0d58291d62548
saishav <saishav7@gmail.com>
2014-10-27 13:21:39 +1100

    changes for preparing status

--- src/Controller.java
+++ src/Controller.java
@@ -13,6 +13,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriBuilder;
 
 import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.GenericType;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.config.ClientConfig;
